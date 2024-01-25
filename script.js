af3f39c963a71a47d2e4935a1d10ba824a0411f3
Person098123 <tameem.mahmoud23@student.cart.org>
2024-01-25 10:40:56 -0800

    finished recursion and min exercise and are now on the character counting exercise.

---
+++ script.js
@@ -0,0 +1,40 @@
+
+//minimum excercise
+function min(a, b){
+    if(a < b) return a;
+       else return b;   
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+
+//Recursion excercise
+
+function isEven(n){
+    if(n % 2 == 0) return true;
+    else return false;
+}
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → false
+
+//Bean counter
+
+function countBs(input){
+let n=input;
+let output=0;
+
+for(i=0; i<n.length(); i++){
+    if n[i]
+}
+}
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
